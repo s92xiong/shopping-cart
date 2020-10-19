9f415327b837cadada5afced119cfabe39b6d384
Sheuh Xiong <sheuhxiong@gmail.com>
2020-10-18 23:12:43 -0400

    Key:value pair is incremented by 1 when ADD TO CART is pressed

--- src/App.js
+++ src/App.js
@@ -19,15 +19,88 @@ import performanceBar15kg from './images/eleiko-performance-weightlifting-bar-wo
 const App = (props) => {
   const [numberOfCartItems, setNumberOfCartItems] = useState(0);
   const [itemCount, setItemCount] = useState(1);
-  // const [totalPrice, setTotalPrice] = useState(0);
- 
+  const [arrayOfItems, setArrayOfItems] = useState([
+    {
+      price: 1110,
+      count: 0,
+      url: "/equipment/competition-bar-20kg-men-id-0",
+    },
+    {
+      price: 1110,
+      count: 0,
+      url: "/equipment/competition-bar-15kg-women-id-1",
+    },
+    {
+      price: 915,
+      count: 0,
+      url: "/equipment/training-bar-20kg-men-id-2",
+    },
+    {
+      price: 915,
+      count: 0,
+      url: "/equipment/training-bar-15kg-women-id-3",
+    },
+    {
+      price: 776,
+      count: 0,
+      url: "/equipment/performance-bar-20kg-men-id-4",
+    },
+    {
+      price: 776,
+      count: 0,
+      url: "/equipment/performance-bar-15kg-women-id-5",
+    },
+  ]);
+
   const decrementItemCount = () => (itemCount === 1) ? 1 : setItemCount(itemCount - 1);
   const incrementItemCount = () => setItemCount(itemCount + 1);
   const handleChange = (e) => e.target.value;
   const handleSubmit = (e) => {
     setNumberOfCartItems(numberOfCartItems + itemCount);
     setItemCount(1);
+    arrayOfItems.forEach((item, index) =>  {
+      if (window.location.href.includes(item.url)) {
+        console.log(`The current URL matches the object item URL: ${item.url}`);
+        const newObj = [...arrayOfItems]
+        newObj[index].count++;
+        setArrayOfItems(newObj);
+        console.table(newObj);
+      }
+    });
   };
+
+  // let currentURL = window.location.href;
+  // if (currentURL.includes(arrayOfItems.item1.url)) {
+  //   console.log(`The URL matches the ${arrayOfItems.item1.url}`);
+  // } else if (currentURL.includes(arrayOfItems.item2.url)) {
+  //   console.log(`The URL matches the ${arrayOfItems.item2.url}`);
+  // } else if (currentURL.includes(arrayOfItems.item3.url)) {
+  //   console.log(`The URL matches the ${arrayOfItems.item3.url}`);
+  // } else if (currentURL.includes(arrayOfItems.item4.url)) {
+  //   console.log(`The URL matches the ${arrayOfItems.item4.url}`);
+  // } else if (currentURL.includes(arrayOfItems.item5.url)) {
+  //   console.log(`The URL matches the ${arrayOfItems.item5.url}`);
+  // } else if (currentURL.includes(arrayOfItems.item6.url)) {
+  //   console.log(`The URL matches the ${arrayOfItems.item6.url}`);
+  // }
+
+  // const expr = window.location.href;
+  // switch (expr) {
+  //   case arrayOfItems.item1.url: 
+  //     console.log("The URL matches the given URL!");
+  //     break;
+  //   default:
+  //     console.log("No expressions!");
+  // }
+
+  // const objInfo = {
+  //   barID0: "id-0",
+  //   barID1: "id-1",
+  //   barID2: "id-2",
+  //   barID3: "id-3",
+  //   barID4: "id-4",
+  //   barID5: "id-5",
+  // }
 
   return (
     <div className="App">
@@ -39,7 +112,7 @@ const App = (props) => {
         <Route exact path="/cart" component={ShoppingCart} />
                 
         <Route
-          exact path="/equipment/competition-bar-20kg-men"
+          exact path={arrayOfItems[0].url}
           render={(props) => (<ShopItemProps 
             image={competitionBar20kg}
             itemName="IWF WEIGHTLIFTING COMPETITION BAR - 20 KG, MEN"
@@ -53,7 +126,7 @@ const App = (props) => {
         />
 
         <Route
-          exact path="/equipment/competition-bar-15kg-women"
+          exact path={arrayOfItems[1].url}
           render={(props) => (<ShopItemProps 
             image={competitionBar15kg}
             itemName="IWF WEIGHTLIFTING COMPETITION BAR - 15 KG, WOMEN"
@@ -67,7 +140,7 @@ const App = (props) => {
         />
 
         <Route
-          exact path="/equipment/training-bar-20kg-men"
+          exact path={arrayOfItems[2].url}
           render={(props) => (<ShopItemProps 
             image={trainingBar20kg}
             itemName="IWF WEIGHTLIFTING TRAINING BAR - 20 KG, MEN"
@@ -81,7 +154,7 @@ const App = (props) => {
         />
 
         <Route
-          exact path="/equipment/training-bar-15kg-women"
+          exact path={arrayOfItems[3].url}
           render={(props) => (<ShopItemProps 
             image={trainingBar15kg}
             itemName="IWF WEIGHTLIFTING TRAINING BAR - 15 KG, WOMEN"
@@ -95,7 +168,7 @@ const App = (props) => {
         />
 
         <Route
-          exact path="/equipment/performance-bar-20kg-men"
+          exact path={arrayOfItems[4].url}
           render={(props) => (<ShopItemProps 
             image={performanceBar20kg}
             itemName="WEIGHTLIFTING PERFORMANCE BAR - 20 KG, MEN"
@@ -109,7 +182,7 @@ const App = (props) => {
         />
 
         <Route
-          exact path="/equipment/performance-bar-15kg-women"
+          exact path={arrayOfItems[5].url}
           render={(props) => (<ShopItemProps 
             image={performanceBar15kg}
             itemName="WEIGHTLIFTING PERFORMANCE BAR - 15 KG, WOMEN"
--- src/components/Equipment.js
+++ src/components/Equipment.js
@@ -12,12 +12,12 @@ const Equipment = () => {
     <div className="component equipment-component">
       <h1 className="weightlifting-header">WEIGHTLIFTING BARBELLS</h1>
       <div className="shop-items">
-        <ShopItem link="/equipment/competition-bar-20kg-men" image={competitionBar20kg} itemName="IWF Weightlifting Competition Bar - 20kg, men" itemPrice="$1,100.00 USD" />
-        <ShopItem link="/equipment/competition-bar-15kg-women" image={competitionBar15kg} itemName="IWF Weightlifting Competition Bar - 15kg, women" itemPrice="$1,100.00 USD" />
-        <ShopItem link="/equipment/training-bar-20kg-men" image={trainingBar20kg} itemName="IWF Weightlifting Training Bar - 20kg, men" itemPrice="$915.00 USD" />
-        <ShopItem link="/equipment/training-bar-15kg-women" image={trainingBar15kg} itemName="IWF Weightlifting Training Bar - 15kg, women" itemPrice="$915.00 USD" />
-        <ShopItem link="/equipment/performance-bar-20kg-men" image={performanceBar20kg} itemName="Performance Weightlifting Bar - 20kg, men" itemPrice="$776.00 USD" />
-        <ShopItem link="/equipment/performance-bar-15kg-women" image={performanceBar15kg} itemName="Performance Weightlifting Bar - 15kg, women" itemPrice="$776.00 USD" />
+        <ShopItem link="/equipment/competition-bar-20kg-men-id-0" image={competitionBar20kg} itemName="IWF Weightlifting Competition Bar - 20kg, men" itemPrice="$1,100.00 USD" />
+        <ShopItem link="/equipment/competition-bar-15kg-women-id-1" image={competitionBar15kg} itemName="IWF Weightlifting Competition Bar - 15kg, women" itemPrice="$1,100.00 USD" />
+        <ShopItem link="/equipment/training-bar-20kg-men-id-2" image={trainingBar20kg} itemName="IWF Weightlifting Training Bar - 20kg, men" itemPrice="$915.00 USD" />
+        <ShopItem link="/equipment/training-bar-15kg-women-id-3" image={trainingBar15kg} itemName="IWF Weightlifting Training Bar - 15kg, women" itemPrice="$915.00 USD" />
+        <ShopItem link="/equipment/performance-bar-20kg-men-id-4" image={performanceBar20kg} itemName="Performance Weightlifting Bar - 20kg, men" itemPrice="$776.00 USD" />
+        <ShopItem link="/equipment/performance-bar-15kg-women-id-5" image={performanceBar15kg} itemName="Performance Weightlifting Bar - 15kg, women" itemPrice="$776.00 USD" />
       </div>
     </div>
   );
