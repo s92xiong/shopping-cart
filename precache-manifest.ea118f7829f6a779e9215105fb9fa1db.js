723a17b5937881a7ff8b1d3e5b56b01517a55988
Sheuh Xiong <sheuhxiong@gmail.com>
2020-10-22 13:32:53 -0400

    Updates

---
+++ precache-manifest.ea118f7829f6a779e9215105fb9fa1db.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ed2511da873cba30ee5afb4490dd29",
+    "url": "/s92xiong/shopping-cart.git/index.html"
+  },
+  {
+    "revision": "f4d44890b60ed3bc6aa3",
+    "url": "/s92xiong/shopping-cart.git/static/css/main.d44b3235.chunk.css"
+  },
+  {
+    "revision": "21136a81af961b82ff9e",
+    "url": "/s92xiong/shopping-cart.git/static/js/2.7fd42361.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/s92xiong/shopping-cart.git/static/js/2.7fd42361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d44890b60ed3bc6aa3",
+    "url": "/s92xiong/shopping-cart.git/static/js/main.7acf9746.chunk.js"
+  },
+  {
+    "revision": "3046cee84dbd50692aad",
+    "url": "/s92xiong/shopping-cart.git/static/js/runtime-main.968d38db.js"
+  },
+  {
+    "revision": "e0e7a973acd375f88da5dfdd9605b667",
+    "url": "/s92xiong/shopping-cart.git/static/media/barbell-image.e0e7a973.jpg"
+  },
+  {
+    "revision": "573691dda2e4478c652388d7eb8c3670",
+    "url": "/s92xiong/shopping-cart.git/static/media/eleiko-iwf-weightlifting-competition-bar-women.573691dd.jpg"
+  },
+  {
+    "revision": "45c1111fc4518cc89c14712cedb2d295",
+    "url": "/s92xiong/shopping-cart.git/static/media/eleiko-iwf-weightlifting-competition-bar.45c1111f.jpg"
+  },
+  {
+    "revision": "57ccfd92f5f3cb1aab06fd039152f671",
+    "url": "/s92xiong/shopping-cart.git/static/media/eleiko-iwf-weightlifting-training-bar-women.57ccfd92.jpg"
+  },
+  {
+    "revision": "5f8bf97f5e493b4cbe96a00e3f5e2577",
+    "url": "/s92xiong/shopping-cart.git/static/media/eleiko-iwf-weightlifting-training-bar.5f8bf97f.jpg"
+  },
+  {
+    "revision": "0feac52a2cf98aaff8b64bdccaed2b28",
+    "url": "/s92xiong/shopping-cart.git/static/media/eleiko-performance-weightlifting-bar-women.0feac52a.jpg"
+  },
+  {
+    "revision": "44f299c0011ecee0d8dab587df6d06fd",
+    "url": "/s92xiong/shopping-cart.git/static/media/eleiko-performance-weightlifting-bar.44f299c0.jpg"
+  },
+  {
+    "revision": "b2ca0cf51b8e3561e2aa9f37e1c0aca8",
+    "url": "/s92xiong/shopping-cart.git/static/media/loaded-barbell.b2ca0cf5.png"
+  },
+  {
+    "revision": "cba6aded55e917c6f5228adfc4cab72b",
+    "url": "/s92xiong/shopping-cart.git/static/media/loaded-barbell2.cba6aded.png"
+  },
+  {
+    "revision": "80ed1c44ae4566de841f1261d9a7b874",
+    "url": "/s92xiong/shopping-cart.git/static/media/nature-image.80ed1c44.png"
+  }
+]);
