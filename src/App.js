649187d01b926ffbeb0ff35ac832e895c6975ccf
Sheuh Xiong <sheuhxiong@gmail.com>
2020-10-22 14:17:06 -0400

    Added ...props

--- src/App.js
+++ src/App.js
@@ -18,37 +18,37 @@ const App = () => {
       name: "IWF WEIGHTLIFTING COMPETITION BAR - 20 KG, MEN",
       price: 1110,
       count: 0,
-      url: "/equipment/competition-bar-20kg-men-id-0",
+      url: "/#/equipment/competition-bar-20kg-men-id-0",
     },
     {
       name: "IWF WEIGHTLIFTING COMPETITION BAR - 15 KG, WOMEN",
       price: 1110,
       count: 0,
-      url: "/equipment/competition-bar-15kg-women-id-1",
+      url: "/#/equipment/competition-bar-15kg-women-id-1",
     },
     {
       name: "IWF WEIGHTLIFTING TRAINING BAR - 20 KG, MEN",
       price: 915,
       count: 0,
-      url: "/equipment/training-bar-20kg-men-id-2",
+      url: "/#/equipment/training-bar-20kg-men-id-2",
     },
     {
       name: "IWF WEIGHTLIFTING TRAINING BAR - 15 KG, WOMEN",
       price: 915,
       count: 0,
-      url: "/equipment/training-bar-15kg-women-id-3",
+      url: "/#/equipment/training-bar-15kg-women-id-3",
     },
     {
       name: "WEIGHTLIFTING PERFORMANCE BAR - 20 KG, MEN",
       price: 776,
       count: 0,
-      url: "/equipment/performance-bar-20kg-men-id-4",
+      url: "/#/equipment/performance-bar-20kg-men-id-4",
     },
     {
       name: "WEIGHTLIFTING PERFORMANCE BAR - 15 KG, WOMEN",
       price: 776,
       count: 0,
-      url: "/equipment/performance-bar-15kg-women-id-5",
+      url: "/#/equipment/performance-bar-15kg-women-id-5",
     },
   ]);
 
@@ -146,7 +146,7 @@ const App = () => {
 
         <Route
           exact path="/cart"
-          render={(props) => (<ShoppingCart 
+          render={(props) => (<ShoppingCart {...props} 
             image={imageArray}
             numberOfCartItems={numberOfCartItems}
             array={arrayOfItems}
@@ -159,7 +159,7 @@ const App = () => {
                 
         <Route
           exact path={arrayOfItems[0].url}
-          render={(props) => (<ShopItemProps 
+          render={(props) => (<ShopItemProps {...props} 
             image={imageArray[0]}
             itemName={arrayOfItems[0].name}
             itemPrice="$1,100.00 USD"
@@ -173,7 +173,7 @@ const App = () => {
 
         <Route
           exact path={arrayOfItems[1].url}
-          render={(props) => (<ShopItemProps 
+          render={(props) => (<ShopItemProps {...props} 
             image={imageArray[1]}
             itemName={arrayOfItems[1].name}
             itemPrice="$1,100.00 USD"
@@ -187,7 +187,7 @@ const App = () => {
 
         <Route
           exact path={arrayOfItems[2].url}
-          render={(props) => (<ShopItemProps 
+          render={(props) => (<ShopItemProps {...props} 
             image={imageArray[2]}
             itemName={arrayOfItems[2].name}
             itemPrice="$915.00 USD"
@@ -201,7 +201,7 @@ const App = () => {
 
         <Route
           exact path={arrayOfItems[3].url}
-          render={(props) => (<ShopItemProps 
+          render={(props) => (<ShopItemProps {...props} 
             image={imageArray[3]}
             itemName={arrayOfItems[3].name}
             itemPrice="$915.00 USD"
@@ -215,7 +215,7 @@ const App = () => {
 
         <Route
           exact path={arrayOfItems[4].url}
-          render={(props) => (<ShopItemProps 
+          render={(props) => (<ShopItemProps {...props} 
             image={imageArray[4]}
             itemName={arrayOfItems[4].name}
             itemPrice="$776.00 USD"
@@ -229,7 +229,7 @@ const App = () => {
 
         <Route
           exact path={arrayOfItems[5].url}
-          render={(props) => (<ShopItemProps 
+          render={(props) => (<ShopItemProps {...props} 
             image={imageArray[5]}
             itemName={arrayOfItems[5].name}
             itemPrice="$776.00 USD"
