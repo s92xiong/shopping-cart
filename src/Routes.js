9d4ec77b44b45055a1fbe56ff8213088e746c51d
Sheuh Xiong <sheuhxiong@gmail.com>
2020-11-14 01:00:13 -0500

    using Hash Router

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const Routes = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
